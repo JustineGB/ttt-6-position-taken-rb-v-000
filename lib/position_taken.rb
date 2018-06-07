4b226231aab0e551b8997e78657a6e6185201e21
Justine Bricknell <justinegayton@gmail.com>
2018-06-07 19:10:19 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,11 +1,5 @@
 def position_taken?(board, index)
   if board[position] == " "
      false
-     elsif board[position] == ""
-     false
-     elsif  board[position] == nil
-     false
-     else   board[position] == "X" || "O"
-     true
 end
 end
